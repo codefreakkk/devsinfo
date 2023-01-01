71e1cd281e6da9addc4163c52db3aaabf115f02d
harsh said <harshsaid558@gmail.com>
2023-01-02 00:51:56 +0530

    initial commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../css/style.css"
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar_heading'>
+            C
+        </div>
+        <div className='sibebar_main'>
+            <div className='main'>H</div>
+            <div className='main'>H</div>
+            <div className='main'>H</div>
+            <div className='main'>H</div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
