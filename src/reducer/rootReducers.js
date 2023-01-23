3eb8b249f58de066eb2408a6719a314965dba143
codefreak <harshsaid558@gmail.com>
2023-01-24 00:23:55 +0530

    changes done

--- src/reducer/rootReducers.js
+++ src/reducer/rootReducers.js
@@ -1,10 +1,8 @@
 import {combineReducers} from "redux";
 import toggleSidebar from "./sidebarReducer";
-import isLoggedIn from "./loginReducer";
 
 const rootReducers = combineReducers({
     toggleSidebar,
-    isLoggedIn,
 })
 
 export default rootReducers;
